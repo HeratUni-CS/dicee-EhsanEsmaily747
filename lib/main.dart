44e58575ff1a3ebbeaa2ce1e02eab3039427b58e
EhsanEsmaily747 <ehsanesmaily747@gmail.com>
2023-05-05 16:47:03 +0430

    dice page added

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,45 @@ void main() {
         ),
         ),
       ),
-      body:Container(),
+      body:const DicePage(),
     )
   ));
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  int leftdice=1;
+  int rightdice=1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(onPressed:(){
+              print("hello");
+            } ,
+             child:Image.asset('images/dice$leftdice.png')
+              ),
+            ),
+          Expanded(
+            child: TextButton(onPressed:(){
+              print("hello");
+            } ,
+             child:Image.asset('images/dice$rightdice.png')
+              ),
+            ),
+
+        ],
+      ),
+    );
+  }
 }
