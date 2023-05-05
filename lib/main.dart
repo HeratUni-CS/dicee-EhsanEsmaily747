b41ea68ab21516d2ce3cd6a5628f19e7551631b5
EhsanEsmaily747 <ehsanesmaily747@gmail.com>
2023-05-05 16:45:16 +0430

    intiail commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text('Dice'),
-        ),
-        body: MyApp(),
-      ),
-    ),
-  );
-}
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+void main() {
+  runApp( MaterialApp(
+      debugShowCheckedModeBanner: false,
+    home:Scaffold(
+    backgroundColor: Colors.blue[300],
+      appBar: AppBar(
+        title:const Center(
+          child:Text(
+            'Dice',
+            style: TextStyle(
+              fontSize: 30.0
+            ),
+        ),
+        ),
+      ),
+      body:Container(),
+    )
+  ));
 }
